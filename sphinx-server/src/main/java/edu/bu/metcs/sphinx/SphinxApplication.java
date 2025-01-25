22aeb072b3255718fc4aaacf13730b8585fc6fe5
vishal <vishal2@bu.edu>
2025-01-25 16:32:23 +0530

    add spring boot project

---
+++ sphinx-server/src/main/java/edu/bu/metcs/sphinx/SphinxApplication.java
@@ -0,0 +1,13 @@
+package edu.bu.metcs.sphinx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SphinxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SphinxApplication.class, args);
+	}
+
+}
