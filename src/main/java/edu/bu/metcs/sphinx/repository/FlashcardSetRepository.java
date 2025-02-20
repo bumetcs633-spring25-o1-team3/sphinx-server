35a4507677c1dc86aeb635e96e4b5c7fba84418c
vishal <vishal2@bu.edu>
2025-02-20 20:05:34 +0530

    update all endpoints with ownership and verification

--- src/main/java/edu/bu/metcs/sphinx/repository/FlashcardSetRepository.java
+++ src/main/java/edu/bu/metcs/sphinx/repository/FlashcardSetRepository.java
@@ -3,8 +3,11 @@ package edu.bu.metcs.sphinx.repository;
 import edu.bu.metcs.sphinx.model.FlashcardSet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface FlashcardSetRepository extends JpaRepository<FlashcardSet, UUID> {
+
+    List<FlashcardSet> findByOwnerId(UUID ownerId);
 
 }
